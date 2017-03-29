5582a8038a7d6b63d9840ddb37a65d1a6d38a1e1
pouet <nchrupal@student.42.fr>
2017-03-29 12:16:31 +0200

    ocaml
    Signed-off-by: pouet <nchrupal@student.42.fr>

--- ocaml/solve.ml
+++ ocaml/solve.ml
@@ -1,5 +1,12 @@
 open Printf
 
+(*
+ * Module Pqueue handle a priority queue
+ * The module MUST be mutable
+ *
+ * -> create : create an empty Pqueue
+*)
+
 module Pqueue = struct
     exception Pqueue_empty
 
@@ -10,8 +17,14 @@ module Pqueue = struct
         | []    -> true
         | _     -> false
 
+    let rec is_present f el = function
+        | []             -> false
+        | (_, e) :: t    ->
+                if f el e = true then true
+                else is_present f el t
+
     let pop = function
-        | (_, e) :: t    -> (e, t)
+        | (_, e) :: t    -> (e, ref t)
         | []        -> raise Pqueue_empty
 
     let rec push prio el = function
@@ -31,10 +44,11 @@ end
 type point = int * int
 
 type node = {
-    grid    : int array array;
-    f       : int;
-    g       : int;
-    h       : int;
+    parent      : int * int;
+    pos         : int * int;
+    mutable f   : int;
+    mutable g   : int;
+    mutable h   : int;
 }
 
 let get_empty_case grid =
@@ -71,56 +85,113 @@ let play_move grid (line, col) (movl, movc) =
 let undo_move grid src move =
     play_move grid src move
 
-let print_el (a, b) =
-    string_of_int a ^ " " ^ string_of_int b
+let h_manhattan (ax, ay) (bx, by) =
+    abs (ax - bx) + abs (ay - by)
 
-let solve grid =
-    let empty = get_empty_case grid
-    and moves = [|
+(*
+ * current -> int * int | empty case
+ * opened  -> 
+*)
+
+let get_neighbors grid (l, c as pt) =
+    let moves = [|
         (1, 0);
         (0, 1);
         (-1, 0);
         (0, -1);
     |]
     in
+    Array.fold_left (fun acc (a, b as mv) -> if is_move_valid grid pt mv = true then
+        (l + a, c + b) :: acc else acc) [] moves
 
-    let opened = Pqueue.create
-    and closed = Pqueue.create
+(* TODO: rajouter tailles <> 3 *)
+let is_solved grid =
+    let grid' = [|
+        [| 1; 2; 3 |];
+        [| 4; 5; 6 |];
+        [| 7; 8; 0 |]
+        |]
+    in
+    grid = grid'
+
+let solve grid =
+    let current = get_empty_case grid
+    and opened = ref Pqueue.create
+    and closed = ref Pqueue.create
     in
 
+    let start = {
+        parent  = (-1, -1);
+        pos     = current;
+        f       = 0;
+        g       = 0;
+        h       = 0;
+    }
+    in
+
+
+    opened := Pqueue.push 0 start !opened;
+
+    let ok, opened = Pqueue.pop !opened
+    in
+
+    printf "%d\n" (fst ok.parent);
+
+    let neigh = get_neighbors grid current
+    in
+
+    List.iter (fun (a, b) -> printf "|> %d %d\n" a b) neigh;
+
+    while Pqueue.is_empty !opened = false do
+        let tmp, opened = Pqueue.pop !opened in
+        closed := Pqueue.push tmp.h tmp !closed
+    done
+
+(*
+    let rec loop opened closed pt = 
+        if Pqueue.is_empty opened = false then begin
+            let (l, c as pt), opened = Pqueue.pop opened in
+            if is_solved grid then print_endline "Wahou !";
+            loop opened closed
+        end
+    in
+    loop opened closed;
+*)
+(*
+    let start = {
+        parent  = grid;
+        f       = 0;
+        g       = 0;
+        h       = h_manhattan (0, 0) (1, 2);
+    }
+    in
+*)
+
+(*
     let opened = Pqueue.push 5 (21, 42) opened in
     let opened = Pqueue.push 6 (12, 98) opened in
     let opened = Pqueue.push 1 (0, 3) opened in
     let opened = Pqueue.push 2 (55, 1234) opened in
+*)
 
-    Pqueue.print print_el opened;
 
-    let tmp = Pqueue.pop opened in
-    let opened = snd tmp
-    and n, m = fst tmp
-    in
+
+(*
+    Pqueue.print (fun (a, b) -> string_of_int a ^ " " ^ string_of_int b) opened;
+*)
+
+(*
+    let (n, m), opened = Pqueue.pop opened in
     printf "%d %d\n" n m;
 
     if Pqueue.is_empty opened then print_endline "empty"
     else print_endline "not empty";
-
-        (*
-let opened = Hashtbl.create 997
-and closed = Hashtbl.create 997
-in
-
-let a 
-
-let start = {
-    grid    = grid;
-    f       = 0;
-    g       = 1;
-    h       = 0;
-}
-in
+    if Pqueue.is_present (fun el e -> el = e) (55, 1234) opened then print_endline "present"
+    else print_endline "not present"
 *)
 
 
+(*
         printf "%d %d\n" (fst empty) (snd empty);
         play_move grid empty moves.(0);
         let empty = get_empty_case grid
@@ -130,6 +201,7 @@ in
         let empty = get_empty_case grid
         in
         printf "%d %d\n" (fst empty) (snd empty)
+*)
 
 let _ =
     let grid = [|
@@ -137,5 +209,23 @@ let _ =
         [| 3; 0; 1 |];
         [| 4; 2; 6 |]
         |]
-        in
-        solve grid
+    in
+(*
+    let grid = [|
+        [| 1; 2; 3 |];
+        [| 4; 5; 6 |];
+        [| 7; 8; 0 |]
+        |]
+    in
+*)
+(*
+    let grid' = [|
+        [| 8; 7; 5 |];
+        [| 3; 0; 1 |];
+        [| 4; 2; 3 |]
+        |]
+    in
+    if grid = grid' then print_endline "same"
+    else print_endline "not same";
+*)
+    solve grid
